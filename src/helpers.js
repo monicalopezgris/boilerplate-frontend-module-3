9ac360012eff185c0a0d94061aad3fb2ea2f54ad
monicalopezgris <monicalopezgris@gmail.com>
2019-05-03 21:01:12 +0200

    add helpers file

---
+++ src/helpers.js
@@ -0,0 +1,11 @@
+
+export function getNewRef(ref) {
+  const num = Number(ref.slice(-2)) + 1;
+  let newRef = ref.slice(0, ref.length - 2);
+  newRef += num;
+  return newRef;
+}
+
+export function aa() {
+  return aa;
+}
