66116b618670dbb38e1015634e70c75773902848
monicalopezgris <monicalopezgris@gmail.com>
2019-05-16 21:11:51 +0200

    add clients list and new

--- src/App.js
+++ src/App.js
@@ -2,32 +2,43 @@
 import React, { Component } from 'react';
 import { Switch } from 'react-router-dom';
 
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+import { GlobalStyle, theme } from './styles/variables'
 import Navbar from './components/Navbar';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dash from './pages/Dash';
 import Bill from './pages/Bill';
+import Client from './pages/Client';
+import Company from './pages/Company';
 
 import PrivateRoute from './components/PrivateRoute';
 import AnonRoute from './components/AnonRoute';
 import AuthProvider from './lib/AuthProvider';
 
-
 class App extends Component {
   render() {
     return (
       <AuthProvider>
-        <div className="container">
-          <h1>Billy</h1>
-          <Navbar />
-          <Switch>
-            <AnonRoute path="/signup" component={Signup} />
-            <AnonRoute path="/login" component={Login} />
-            <PrivateRoute exact path="/:id" component={Bill} />
-            <PrivateRoute path="/new" component={Bill} />
-            <PrivateRoute path="/" component={Dash} />
-          </Switch>
-        </div>
+        <ThemeProvider theme={theme}>
+          <div className="container">
+            <Reset />
+            <PrivateRoute path="/" component={Navbar} />
+            <Switch>
+              <AnonRoute exact path="/signup" component={Signup} />
+              <AnonRoute exact path="/login" component={Login} />
+              <PrivateRoute exact path="/:id" component={Bill} />
+              <PrivateRoute exact path="/new" component={Bill} />
+              <PrivateRoute exact path="/" component={Dash} />
+              <PrivateRoute exact path="/company/:id" component={Company} />
+              <PrivateRoute exact path="/company/new" component={Company} />
+              <PrivateRoute exact path="/client/:id" component={Client} />
+              <PrivateRoute exact path="/client/new" component={Client} />
+            </Switch>
+            <GlobalStyle />
+          </div>
+        </ThemeProvider>
       </AuthProvider>
     );
   }
---
+++ src/components/clients/ClientList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { withAuth } from '../../lib/AuthProvider';
+import clientService from '../../lib/client-service';
+import ClientListItem from './ClientListItem';
+
+const Wrapper = styled.div`
+  height: 100%;
+  background-color: ${props => props.theme.color.secondaryColor};
+`;
+
+class ClientList extends Component {
+  state = {
+    clients: [],
+  }
+
+  async componentDidMount() {
+    const clients = await clientService.get();
+    this.setState({ clients: clients.data });
+  }
+
+  onDelete = (id) => {
+    clientService.delete(id);
+  }
+
+  render() {
+    const { clients } = this.state;
+    return (
+      <Wrapper>
+        <Link to="/client/new"><button type="button">Add</button></Link>
+        {
+          clients.map((client) => {
+            const { _id: id, name } = client;
+            return <ClientListItem key={id} itemData={{ id, name }} onDelete={this.onDelete} />;
+          })
+        }
+      </Wrapper>
+    );
+  }
+}
+
+export default withAuth(ClientList);
