a32a36d846c01ce25ed52fbbf8d26600d48e7b6b
monicalopezgris <monicalopezgris@gmail.com>
2019-05-16 21:12:38 +0200

    add client service lib

---
+++ src/lib/client-service.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+
+class ClientsService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_URL}/api`,
+      withCredentials: true,
+    });
+  }
+
+  get() {
+    return this.api.get('/clients')
+      .then(({ data }) => ({ data }));
+  }
+
+  getById(id) {
+    return this.api.get(`/clients/${id}`)
+      .then(({ data }) => ({ data }));
+  }
+
+  add(inputData) {
+    return this.api.post('/clients', inputData)
+      .then(({ data }) => data);
+  }
+
+  update(id, inputData) {
+    return this.api.put(`/clients/${id}`, inputData)
+      .then(({ data }) => data);
+  }
+
+  delete(id) {
+    return this.api.delete(`/clients/${id}`)
+      .then(({ data }) => data);
+  }
+}
+
+const clientsService = new ClientsService();
+
+export default clientsService;
