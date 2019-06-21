4c2b5b4f9e0d8c2d19157bb5620776e078621ae2
monicalopezgris <monicalopezgris@gmail.com>
2019-06-22 00:06:11 +0200

    UPDATE nweDoc form, now is vanilla react

--- src/components/bill/NewDoc.js
+++ src/components/bill/NewDoc.js
@@ -1,51 +1,77 @@
+/* eslint-disable indent */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import {
-  Formik, Field, FieldArray, Form, ErrorMessage, option,
-} from 'formik';
-import { withRouter } from 'react-router-dom';
-import { billSchema } from '../../lib/validationSchemas';
 import ids from 'short-id';
+import { withRouter } from 'react-router-dom';
 import doc from '../../lib/doc-service';
-
-const Formu = styled(Form)`
-  margin: 0;
-  width: 100%;
-  background-color:${props => props.theme.color.secondaryColor};
-  font-size: 1.3rem;
-  display: flex;
-  flex-direction: column;
-  padding: 0 15% 0 15%;
-  color: black;
-`;
-const Label = styled.span`
-  text-align: center;
-  margin: 5% 0;
-`;
-const Heading = styled.span`
-  font-size:1.5;
-  font-weight:bold;
-  text-align: center;
-  margin: 5% 0;
-`;
-const Fieldu = styled(Field)`
-  border-radius: 10px;
-  border: 2px solid transparent;
-  font-size: 1rem;
-  text-align: center;
-  &: hover{
-    border-color: ${props => props.theme.color.primaryColor};
-  }
-  &: focus{
-    border-color: ${props => props.theme.color.primaryColor};
-  }
-  padding: 3% 5%;
-`;
+import auth from '../../lib/auth-service';
 
 class NewDocForm extends Component {
-  state = {
-    savedClient: false,
-    error: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClient: true,
+      clients: [],
+      ref: undefined,
+      selectedClient: undefined,
+      name: undefined,
+      cif: undefined,
+      street: undefined,
+      streetNum: undefined,
+      postalCode: undefined,
+      country: undefined,
+      objects: [],
+    };
+  }
+
+  async componentDidMount() {
+    const user = await auth.meData();
+    const { clients } = user.data;
+    this.setState({
+      clients,
+    });
+  }
+
+  handleIsClient = () => {
+    const { isClient } = this.state;
+    if (isClient) {
+      this.setState({
+        selectedClient: undefined,
+      });
+    }
+  }
+
+  handleAddObject = () => {
+    this.setState(prevState => ({
+      objects: [...prevState.objects, { item: '', units: 1, priceUnit: 0 }],
+    }));
+  }
+
+  handleDeleteObject = (event) => {
+    const { objects } = this.state;
+    const { dataset } = event.target;
+    objects.splice(dataset.id, 1);
+    this.setState({
+      objects,
+    });
+  }
+
+  handleInputChange = (event) => {
+    const { className, name } = event.target;
+    if (['item', 'units', 'priceUnits'].includes(className)) {
+      const { value, dataset } = event.target;
+      const objects = [...this.state.objects]
+      objects[dataset.id][name] = value;
+      this.setState({ objects });
+    } else {
+      const { target } = event;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      this.setState({
+        [name]: value,
+      });
+    }
   }
 
   getRef = async (values) => {
@@ -54,10 +80,12 @@ class NewDocForm extends Component {
     return (values);
   }
 
-  handleSubmit = async (values) => {
+  handleSubmit = async (event) => {
+    event.preventDefault();
     const { history } = this.props;
-    await this.getRef(values);
+    let values = this.state;
     try {
+      values = await this.getRef(values);
       const result = await doc.add(values);
       const { _id: id } = result;
       history.push(`/bill/${id}`);
@@ -68,136 +96,149 @@ class NewDocForm extends Component {
     }
   }
 
-  handleChange = () => {
-    const { savedClient } = this.state;
-    if (savedClient) {
-      this.setState({
-        savedClient: false,
-      });
-    } else {
-      this.setState({
-        savedClient: true,
-      });
-    }
-  }
-
   render() {
-    const { error } = this.state;
-    if (!error) {
-      return (
-        <>
-          <Formik
-            initialValues={{ savedClient: true, status: String, items: [] }}
-            validationSchema={billSchema}
-            onSubmit={(values, actions) => {
-              actions.setSubmitting(false);
-              this.handleSubmit(values);
-            }}
-            render={({ values }) => (
-              <Formu>
-                <Field type="hidden" name="ref" />
-                <Heading> Client </Heading>
-                <Label> Saved client?</Label>
-                <Field type="checkbox" name="savedClient" checked={values.savedClient} onChange={this.handleChange} />
-                {this.state.savedClient
-                  ? (
-                    <Field component="select" name="status">
-                      <option value={"client1"}>client1</option>
-                      <option value={"closed"}>client2</option>
-                      <option value={"sended"}>client3</option>
-                    </Field>
-                  )
-                  : (
-                    <>
-                      <Label> Name</Label>
-                      <Fieldu
-                        type="text"
-                        name="name"
-                        placeholder="name"
-                      />
-                      <Label>Cif</Label>
-                      <ErrorMessage name="name" />
-                      <Fieldu
-                        type="text"
-                        name="cif"
-                        placeholder="cif"
-                      />
-                      <ErrorMessage name="cif" />
-                      <Label>Street</Label>
-                      <Fieldu
-                        type="text"
-                        name="street"
-                        placeholder="street"
-                      />
-                      <ErrorMessage name="street" />
-                      <Label>Number</Label>
-                      <Fieldu
-                        type="number"
-                        name="streetNum"
-                        placeholder="Number"
-                      />
-                      <ErrorMessage name="streetNum" />
-                      <Label>PostalCode</Label>
-                      <Fieldu
-                        type="number"
-                        name="postalCode"
-                        placeholder="Postal Code"
-                      />
-                      <ErrorMessage name="postalCode" />
-                      <Label>Country</Label>
-                      <Fieldu
-                        type="text"
-                        name="country"
-                        placeholder="Country"
-                      />
-                      <ErrorMessage name="country" />
-                    </>
-                  )
-                }
-                <FieldArray
-                  name="items"
-                  render={arrayHelpers => (
-                    <div>
-                      {values.items.map((item, index) => (
-                        <div key={index}>
-                          <Label>Item</Label>
-                          <Fieldu name={`items[${index}].item`} placeholder="item" />
-                          <Label>Units</Label>
-                          <Fieldu type="number" name={`items[${index}].units`} placeholder="units" />
-                          <Label>Price Unit</Label>
-                          <Fieldu type="number" name={`items[${index}].priceUnit`} placeholder="priceUnit" />
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.remove(index)}
-                          >
-                            {' '}
-                            -
-                          {' '}
-                          </button>
-                        </div>
-                      ))
-                      }
-                      <button type="button" onClick={index => arrayHelpers.push('')}>
-                        Add an item
-                      </button>
-                    </div>
-                  )}
-                />
-                <Field component="select" name="status">
-                  <option value="draft">Draft</option>
-                  <option value="Closed">Closed</option>
-                </Field>
-                <div>
-                  <button type="submit">Submit</button>
-                </div>
-              </Formu>
-            )
-            }
+    const {
+      ref, name, cif, street, postalCode, streetNum, country, isClient, clients, selectedClient, objects,
+    } = this.state;
+
+    return (
+      <form
+        onChange={this.handleInputChange}
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          type="hidden"
+          value={ref}
+        />
+        <label>
+          Already a client?:
+          <input
+            name="isClient"
+            type="checkbox"
+            checked={isClient}
+            onClick={this.handleIsClient}
+            onChange={this.handleInputChange}
           />
-        </>
-      );
-    }
-    return <div>{error}</div>;
+        </label>
+
+        {isClient ? (
+          <select name="selectedClient" value={selectedClient} onChange={this.handleInputChange}>
+            {
+              clients.map(
+                client => <option key={client._id} value={client._id}>{client.name}</option>,
+              )
+            }
+          </select>
+        ) : (
+            <>
+              <div>Client info</div>
+              <label htmlFor={name}>
+                Name:
+              </label>
+              <input
+                name="name"
+                type="text"
+              />
+              <br />
+              <label htmlFor={cif}>
+                Cif:
+              </label>
+              <input
+                name="cif"
+                type="text"
+              />
+              <br />
+              <label htmlFor={street}>
+                Street:
+              </label>
+              <input
+                name="street"
+                type="text"
+              />
+              <br />
+              <label htmlFor={streetNum}>
+                Street Number:
+              </label>
+              <input
+                name="streetNum"
+                type="number"
+              />
+              <br />
+              <label htmlFor={postalCode}>
+                Postal Code:
+              </label>
+              <input
+                name="postalCode"
+                type="number"
+              />
+              <br />
+              <label htmlFor={country}>
+                Country:
+              </label>
+              <input
+                name="country"
+                type="text"
+              />
+            </>
+          )}
+        {objects.length >= 0 ? (
+          objects.map((val, index) => {
+            const itemIndex = `item-${index}`;
+            const unitsIndex = `units-${index}`;
+            const priceUnitIndex = `priceUnits-${index}`;
+            return (
+              <div key={index}>
+                <button
+                  type="button"
+                  data-id={index}
+                  onClick={this.handleDeleteObject}>Delete</button>
+                <label htmlFor={itemIndex}>
+                  Item
+                </label>
+                <input
+                  type="text"
+                  name="item"
+                  data-id={index}
+                  id={itemIndex}
+                  value={objects[index].item}
+                  className="item"
+                />
+                <br />
+                <label htmlFor={unitsIndex}>
+                  Units
+                </label>
+                <input
+                  type="number"
+                  defaultValue={1}
+                  name="units"
+                  data-id={index}
+                  id={unitsIndex}
+                  value={objects[index].units}
+                  className="units"
+                />
+                <br />
+                <label htmlFor={priceUnitIndex}>
+                  Price / Unit
+                </label>
+                <input
+                  type="number"
+                  defaultValue={0}
+                  name="priceUnit"
+                  data-id={index}
+                  id={priceUnitIndex}
+                  value={objects[index].priceUnits}
+                  className="priceUnits"
+                />
+              </div>
+            );
+          })
+        ) : (
+            <div />
+          )}
+        <button type="button" onClick={this.handleAddObject}> Add item</button>
+        <button type="submit">Send</button>
+      </form>
+    );
   }
 }
 
